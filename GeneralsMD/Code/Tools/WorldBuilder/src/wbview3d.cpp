e7feab605b8575ba30432b0ddbdbe64f7f3d0874
Mads Jans <90150876+ViTeXFTW@users.noreply.github.com>
2026-02-17 19:18:26 +0100

    bugfix(worldbuilder): Fix optimized trees not being shown in object placer preview window (#2312)

--- GeneralsMD/Code/Tools/WorldBuilder/src/wbview3d.cpp
+++ GeneralsMD/Code/Tools/WorldBuilder/src/wbview3d.cpp
@@ -29,6 +29,7 @@
 #include "intersec.h"
 #include "W3DDevice/GameClient/W3DAssetManager.h"
 #include "W3DDevice/GameClient/Module/W3DModelDraw.h"
+#include "W3DDevice/GameClient/Module/W3DTreeDraw.h"
 #include "agg_def.h"
 #include "part_ldr.h"
 #include "hanim.h"
@@ -1117,6 +1118,14 @@ AsciiString WbView3d::getBestModelName(const ThingTemplate* tt, const ModelCondi
 			if (md)
 			{
 				return md->getBestModelNameForWB(c);
+			}
+
+			// TheSuperHackers @bugfix ViTeXFTW 15/02/2026 Fix tree objects not showing a preview in
+			// WB object placer. The W3DTreeDraw module stores its model name differently from W3DModelDraw.
+			const W3DTreeDrawModuleData* treeData = mdd ? mdd->getAsW3DTreeDrawModuleData() : nullptr;
+			if (treeData)
+			{
+				return treeData->m_modelName;
 			}
 		}
 	}

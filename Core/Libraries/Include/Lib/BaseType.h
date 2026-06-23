3c6f0e9cb5aeab722acaabf1afd8a2149e51734e
xezon <4720891+xezon@users.noreply.github.com>
2026-06-23 20:58:13 +0200

    chore(basetype): Add addition and subtraction functions to Coord2D, Coord3D, ICoord2D, ICoord3D (#2826)

--- Core/Libraries/Include/Lib/BaseType.h
+++ Core/Libraries/Include/Lib/BaseType.h
@@ -298,7 +298,54 @@ struct Coord2D
 
 	Real toAngle() const;  ///< turn 2D vector into angle (where angle 0 is down the +x axis)
 
+	void add( const Coord2D &a )
+	{
+		x += a.x;
+		y += a.y;
+	}
+
+	void sub( const Coord2D &a )
+	{
+		x -= a.x;
+		y -= a.y;
+	}
+
+	void operator+=( const Coord2D &a )
+	{
+		add(a);
+	}
+
+	void operator-=( const Coord2D &a )
+	{
+		sub(a);
+	}
+
+	void set( const Coord2D &a )
+	{
+		x = a.x;
+		y = a.y;
+	}
+
+	void set( Real ax, Real ay )
+	{
+		x = ax;
+		y = ay;
+	}
 };
+
+inline Coord2D operator+( const Coord2D &a, const Coord2D &b )
+{
+	Coord2D c = a;
+	c.add(b);
+	return c;
+}
+
+inline Coord2D operator-( const Coord2D &a, const Coord2D &b )
+{
+	Coord2D c = a;
+	c.sub(b);
+	return c;
+}
 
 inline Real Coord2D::toAngle() const
 {
@@ -376,7 +423,56 @@ struct ICoord2D
 	}
 
 	Int length() const { return (Int)sqrt( (double)(x*x + y*y) ); }
+	Int lengthSqr() const { return x*x + y*y; }
+
+	void add( const ICoord2D &a )
+	{
+		x += a.x;
+		y += a.y;
+	}
+
+	void sub( const ICoord2D &a )
+	{
+		x -= a.x;
+		y -= a.y;
+	}
+
+	void operator+=( const ICoord2D &a )
+	{
+		add(a);
+	}
+
+	void operator-=( const ICoord2D &a )
+	{
+		sub(a);
+	}
+
+	void set( const ICoord2D &a )
+	{
+		x = a.x;
+		y = a.y;
+	}
+
+	void set( Int ax, Int ay )
+	{
+		x = ax;
+		y = ay;
+	}
 };
+
+inline ICoord2D operator+( const ICoord2D &a, const ICoord2D &b )
+{
+	ICoord2D c = a;
+	c.add(b);
+	return c;
+}
+
+inline ICoord2D operator-( const ICoord2D &a, const ICoord2D &b )
+{
+	ICoord2D c = a;
+	c.sub(b);
+	return c;
+}
 
 struct Region2D
 {
@@ -471,6 +567,16 @@ struct Coord3D
 		z -= a.z;
 	}
 
+	void operator+=( const Coord3D &a )
+	{
+		add(a);
+	}
+
+	void operator-=( const Coord3D &a )
+	{
+		sub(a);
+	}
+
 	void set( const Coord3D &a )
 	{
 		x = a.x;
@@ -507,11 +613,26 @@ struct Coord3D
 	}
 };
 
+inline Coord3D operator+( const Coord3D &a, const Coord3D &b )
+{
+	Coord3D c = a;
+	c.add(b);
+	return c;
+}
+
+inline Coord3D operator-( const Coord3D &a, const Coord3D &b )
+{
+	Coord3D c = a;
+	c.sub(b);
+	return c;
+}
+
 struct ICoord3D
 {
 	Int x, y, z;
 
 	Int length() const { return (Int)sqrt( (double)(x*x + y*y + z*z) ); }
+	Int lengthSqr() const { return x*x + y*y + z*z; }
 
 	void zero()
 	{
@@ -524,7 +645,59 @@ struct ICoord3D
 	{
 		return x == value && y == value && z == value;
 	}
+
+	void add( const ICoord3D &a )
+	{
+		x += a.x;
+		y += a.y;
+		z += a.z;
+	}
+
+	void sub( const ICoord3D &a )
+	{
+		x -= a.x;
+		y -= a.y;
+		z -= a.z;
+	}
+
+	void operator+=( const ICoord3D &a )
+	{
+		add(a);
+	}
+
+	void operator-=( const ICoord3D &a )
+	{
+		sub(a);
+	}
+
+	void set( const ICoord3D &a )
+	{
+		x = a.x;
+		y = a.y;
+		z = a.z;
+	}
+
+	void set( Int ax, Int ay, Int az )
+	{
+		x = ax;
+		y = ay;
+		z = az;
+	}
 };
+
+inline ICoord3D operator+( const ICoord3D &a, const ICoord3D &b )
+{
+	ICoord3D c = a;
+	c.add(b);
+	return c;
+}
+
+inline ICoord3D operator-( const ICoord3D &a, const ICoord3D &b )
+{
+	ICoord3D c = a;
+	c.sub(b);
+	return c;
+}
 
 // For alternative see AABoxClass
 struct Region3D

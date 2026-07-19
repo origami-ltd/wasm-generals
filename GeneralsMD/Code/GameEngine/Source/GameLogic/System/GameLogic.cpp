0555517c17041ed680fb1b9014164cdbe8e4300c
Felipe Braz <fbraz3@gmail.com>
2026-07-19 14:33:09 -0300

    bugfix(gamelogic): Map player index to slot index for CRC validation (#2857)

--- GeneralsMD/Code/GameEngine/Source/GameLogic/System/GameLogic.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameLogic/System/GameLogic.cpp
@@ -2671,7 +2671,8 @@ void GameLogic::processCommandList( CommandList *list )
 				{
 					// TheSuperHackers @bugfix Caball009 14/06/2026 Check if player is still connected,
 					// to avoid spurious mismatches at low CRC intervals, e.g. every frame.
-					if (!TheNetwork->isPlayerConnected(it->first))
+					const Int slotIndex = ThePlayerList->getSlotIndex(it->first);
+					if (slotIndex >= 0 && !TheNetwork->isPlayerConnected(slotIndex))
 						continue;
 
 					const UnsignedInt crc = it->second;
